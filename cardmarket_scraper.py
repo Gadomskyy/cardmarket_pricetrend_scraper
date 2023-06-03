519febfa92e79caea1e231e1ff705f77fb179ff3
Gadomskyy <j_gadomski@hotmail.com>
2023-06-03 22:25:30 +0200

    remove read_csv_column function
    Redundant read_csv_column function has been removed, as reading now takes place inside check_value function

--- cardmarket_scraper.py
+++ cardmarket_scraper.py
@@ -2,18 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-#Read links to cards from csv file
-def read_csv_column(csv_file, column_name):
-    with open(csv_file, 'r') as file:
-        reader = csv.DictReader(file)
-        column_data = []
-        for row in reader:
-            column_data.append(row[column_name])
-    return column_data
-
-link_list = read_csv_column('cardlist.csv', 'Link')
-
-
 def check_column_value(csv_file, column_index, condition_value):
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
@@ -27,7 +15,7 @@ def check_column_value(csv_file, column_index, condition_value):
             if value == condition_value:
                 # Find the price trend element in soup
                 # Price trend is located inside labeled row class, and then in a col-6 colxl-7 class. I use soup find twice to get a list
-                # of all elements with that class name - there are multiple - Price trend information index is 6 for Pokemon, 5 for trainers
+                # of all elements with that class name - there are multiple - Price trend information index is 6 for Pokemon, 5 for trainers (different layout)
                 price_trend_element = soup.find(class_=lambda value: value and value.startswith("labeled row")).find_all(class_=lambda value: value and value.startswith("col-6 col-xl-7"))[6].text
                 print(price_trend_element)
             else:
