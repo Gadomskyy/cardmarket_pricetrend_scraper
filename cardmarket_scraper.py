aeb1a9e196a95b9ddbd7fea76d0d06a27abc5668
Gadomskyy <j_gadomski@hotmail.com>
2023-06-03 22:07:11 +0200

    add reading links for scrape from csv file

--- cardmarket_scraper.py
+++ cardmarket_scraper.py
@@ -1,15 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-#Request URL
-url = "https://www.cardmarket.com/en/Pokemon/Products/Singles/Lost-Origin/Pikachu-V2-LORTG05"
-response = requests.get(url)
+#Read links to cards from csv file
+def read_csv_column(csv_file, column_name):
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        column_data = []
+        for row in reader:
+            column_data.append(row[column_name])
+    return column_data
 
-#Parse the response
-soup = BeautifulSoup(response.content, "html.parser")
+link_list = read_csv_column('cardlist.csv', 'Link')
 
-#Find the price trend element in soup
-#Price trend is located inside labeled row class, and then in a col-6 colxl-7 class. I use soup find twice to get a list
-#of all elements with that class name - there are multiple - Price trend information index is 6
-price_trend_element = soup.find(class_=lambda value: value and value.startswith("labeled row")).find_all(class_=lambda value: value and value.startswith("col-6 col-xl-7"))[6].text
-print(price_trend_element)
+with open('cardlist_result.csv', 'w') as file:
+    for link in link_list:
+        response = requests.get(link)
+        # Parse the response
+        soup = BeautifulSoup(response.content, "html.parser")
+        # Find the price trend element in soup
+        # Price trend is located inside labeled row class, and then in a col-6 colxl-7 class. I use soup find twice to get a list
+        # of all elements with that class name - there are multiple - Price trend information index is 6
+        price_trend_element = soup.find(class_=lambda value: value and value.startswith("labeled row")).find_all(
+            class_=lambda value: value and value.startswith("col-6 col-xl-7"))[6].text
+        print(price_trend_element)
