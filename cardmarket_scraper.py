c7515704ae060026c26e340f2f7574e8b1af65e7
Gadomskyy <j_gadomski@hotmail.com>
2023-06-04 18:40:10 +0200

    Add a list to hold the scraped values
    Also the function's name has been changed from check_column_value to create_pricelist to better show the meaning of the function

--- cardmarket_scraper.py
+++ cardmarket_scraper.py
@@ -2,10 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-def check_column_value(csv_file, column_index, condition_value):
+def create_pricelist(csv_file, column_index, condition_value):
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
         header = next(reader)  # Skip the header row
+
+        #create list to hold all price values
+        pricelist = []
 
         for row in reader:
             value = row[column_index]
@@ -17,12 +20,13 @@ def check_column_value(csv_file, column_index, condition_value):
                 # Price trend is located inside labeled row class, and then in a col-6 colxl-7 class. I use soup find twice to get a list
                 # of all elements with that class name - there are multiple - Price trend information index is 6 for Pokemon, 5 for trainers (different layout)
                 price_trend_element = soup.find(class_=lambda value: value and value.startswith("labeled row")).find_all(class_=lambda value: value and value.startswith("col-6 col-xl-7"))[6].text
-                print(price_trend_element)
+                pricelist.append(price_trend_element)
             else:
                 price_trend_element = soup.find(class_=lambda value: value and value.startswith("labeled row")).find_all(class_=lambda value: value and value.startswith("col-6 col-xl-7"))[5].text
-                print(price_trend_element)
+                pricelist.append(price_trend_element)
+    return pricelist
 
 
 
-with open('cardlist_result.csv', 'w') as file:
-        check_column_value('cardlist.csv', 3, 'Y')
+print(check_column_value('cardlist.csv', 3, 'Y'))
+
